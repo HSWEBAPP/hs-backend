71e37f71f59cad3a1588503868f64ed957f04ecc
HSWEBAPP <hswebapp2025@gmail.com>
2025-08-19 10:00:00 +0530

    Deployment

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.use(express.json())
 
 app.use(cors({
   origin: [
-    "https://hs-frontend-two.vercel.app",  // ✅ correct deployed frontend
-    "http://localhost:3000"                // ✅ for local dev
+    "https://hs-frontend-two.vercel.app",
+    "http://localhost:5173"
   ],
-  credentials: true, // or false if you only use localStorage
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
