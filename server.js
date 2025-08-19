1b9fbe1d3c984b15cdac5fc2b589def38bb19a9d
HSWEBAPP <hswebapp2025@gmail.com>
2025-08-19 11:34:59 +0530

    Deployment

--- server.js
+++ server.js
@@ -1,54 +1,51 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
-import mongoose from 'mongoose'
-import path from "path"
-
-// Routes
 import authRoutes from './routes/auth.routes.js';
-import walletRoutes from './routes/wallet.js';          // balance + shared wallet routes
+import mongoose from 'mongoose'
+import walletRoutes from './routes/wallet.js';
 import adminRoutes from './routes/admin.routes.js';
 import qrCodeRoutes from "./routes/qr.routes.js";
+
 import walletUserRoutes from "./routes/wallet.user.routes.js";
 import walletAdminRoutes from "./routes/wallet.admin.routes.js";
+
+import path from "path";
+
 
 dotenv.config()
 
 const app = express()
+
 app.use(express.json())
-
-// ✅ CORS config
-
 app.use(cors({
   origin: [
-    "https://hs-frontend-two.vercel.app", 
-    "http://localhost:5173"
+    "https://hs-frontend-two.vercel.app/", // your new frontend
+    "http://localhost:5173"   // keep this if you still use old one
   ],
- methods: ["GET", "POST", "PUT", "PATCH", "DELETE",  "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}))
+  credentials: false
+}));
 
 
-// ✅ Test route
+
+// Test route
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-// ✅ Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/wallet', walletRoutes);          // → /api/wallet/balance
-app.use('/api/wallet/user', walletUserRoutes); // → /api/wallet/user/recharge/qr
-app.use('/api/admin/wallet', walletAdminRoutes); // → /api/admin/wallet/...
+app.use('/api/wallet', walletRoutes);
 app.use('/api/admin', adminRoutes);
-app.use('/api/qrcode', qrCodeRoutes);
+app.use("/api/qrcode", qrCodeRoutes);
+app.use("/api/wallet", walletUserRoutes);
+app.use("/api/admin/wallet", walletAdminRoutes);
+app.use("/api/admin", adminRoutes);
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
-
-// ✅ MongoDB
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB error:", err));
 
-// ✅ Start server
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
