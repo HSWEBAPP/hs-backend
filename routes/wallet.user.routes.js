eca4bcda074c3a35536e251d9e0afb621969a403
HSWEBAPP <hswebapp2025@gmail.com>
2025-08-22 20:59:27 +0530

    API changes

--- routes/wallet.user.routes.js
+++ routes/wallet.user.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { authMiddleware } from "../middleware/authMiddleware.js";
-import { submitQRRecharge,getQRRechargeHistory ,getUserTransactions } from "../controllers/wallet.user.controller.js";
+import { submitQRRecharge,getQRRechargeHistory ,getUserTransactions, getMyRechargeRequests } from "../controllers/wallet.user.controller.js";
 import { deductToolUsage } from "../controllers/wallet.user.controller.js";
 
 const router = express.Router();
 
 router.post("/user/recharge/qr", authMiddleware, submitQRRecharge);
-router.get("/recharge/history", authMiddleware, getQRRechargeHistory);
+router.get("/recharge/history", authMiddleware, getMyRechargeRequests);
 router.post("/deduct-tool-usage", authMiddleware, deductToolUsage);
 router.get("/transactions", authMiddleware, getUserTransactions);
 
